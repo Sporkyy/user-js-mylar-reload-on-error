4489cd68e606f45d07a624d8714e539b41b268aa
Todd Sayre <captain.friendly@gmail.com>
2022-05-05 17:04:18 -0400

    First move from gist to proper repo

---
+++ mylar-reload-on-error.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Mylar - Reload on Error
+// @namespace   https://github.com/Sporkyy/
+// @include     *://*:8090*
+// @run-at      document-start
+// @grant       none
+// @version		  1.0
+// @author      Todd Sayre
+// @description	Reloads the page if Mylar throws an error
+// @noframes
+// ==/UserScript==
+
+document.querySelector('h2').textContent.includes('Internal Server Error') &&
+  setTimeout(
+    () => document.location.reload(),
+    Math.trunc(Math.random() * 6 + 5) * 1000,
+  );
